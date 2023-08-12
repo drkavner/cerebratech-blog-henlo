6a362489f72a55ccba86800629f9d86d84dc869a
Dr Amanda Kavner <78941808+drkavner@users.noreply.github.com>
2023-08-12 14:42:40 -0400

    add pages index

---
+++ content/pages/index.js
@@ -0,0 +1,12 @@
+import home from '@/content/pages/home';
+import blog from '@/content/pages/blog';
+import about from '@/content/pages/about';
+import privacy from '@/content/pages/privacy';
+
+const pages = {
+  name: 'pages',
+  label: 'Pages',
+  files: [home, about, blog, privacy],
+};
+
+export default pages;
